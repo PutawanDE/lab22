552ebab0c970767ed197fa4344b896f83386080a
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-03-12 17:33:09 +0700

    Finish lab22

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+int count(int d[], int size){
+	set<int> s1;
+	s1.insert(d, d+size);
+	return s1.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
